a251e0b82b4d308ce2999450c1fd349bb4ffaf2f
dedi <dediirwansyah1925@gmail.com>
2021-08-16 13:43:43 +0700

    menginstall cors dan menUSE nya

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import route from './routes/index.js'
+import cors from 'cors';
+
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/restfull_db',{
@@ -12,6 +14,7 @@ const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.on('open', () => console.log('Database Connected'));
 
+app.use(cors())
 app.use(express.json())
 app.use('/product', route)
 app.listen('3000', () => console.log('Server Running at port :3000'))
