c282ea6d2547c1ec9549cbf3dc46f2a03b1bfbb1
dedi <dediirwansyah1925@gmail.com>
2021-08-16 09:18:28 +0700

    membuat koneksi ke database

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/restfull_db',{
+    useNewUrlParser:true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.on('open', () => console.log('Database Connected'));
 
 app.get('/',(req,res) => {
     res.send('welcome')
