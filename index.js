3d3fb2985043e5209e8fcf0536275f5e87e7a2e4
dedi <dediirwansyah1925@gmail.com>
2021-08-16 09:24:24 +0700

    membuat Routes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
-
+import route from './routes/index.js'
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/restfull_db',{
@@ -12,8 +12,5 @@ const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.on('open', () => console.log('Database Connected'));
 
-app.get('/',(req,res) => {
-    res.send('welcome')
-})
-
+app.use('/product', route)
 app.listen('3000', () => console.log('Server Running at port :3000'))
